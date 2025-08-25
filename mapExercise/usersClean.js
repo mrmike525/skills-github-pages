5005abd794c48097fbd84d9d37871246022d451b
mrmike525 <mrlange892@gmail.com>
2025-08-25 13:49:04 -0500

    Add files via upload

---
+++ mapExercise/usersClean.js
@@ -0,0 +1,35 @@
+const users = [
+	{firstName: "Alice", lastName: "Johnson", points: 120},
+	{firstName: "Bob", lastName: "Smith", points: 99},
+	{firstName: "Charlie", lastName: "Brown", points: 180}
+];
+
+const compiledUsers = users.map(function(fullName){
+	let membershipStatus = fullName.points;
+	if(fullName.points > 100) {
+		membershipStatus = 'Premium'
+	} else {
+		membershipStatus = 'Standard'
+	}
+
+
+    return {
+		fullName: `${fullName.firstName} ${fullName.lastName}`, 
+		membershipStatus: `${membershipStatus}` }
+    });
+
+
+    console.log(compiledUsers);
+
+    let h1 = document.querySelector('h1');
+    let h2 = document.createElement('h2');
+    h2.innerText = "Array with object inside";
+    h1.append(h2)
+    let p = document.createElement('p');
+    const objectAsString = JSON.stringify(compiledUsers);
+    p.innerText = (objectAsString)
+    h1.append(p)
+
+    let color = document.querySelector('p')
+    color.style.color = 'red'
+    color.style.width = '70%'
